319ef138fb3d76ce434fa74cd6318f8483cc6013
mobinaebrahim <mobinaebrahim1385@gmail.com>
2026-07-05 01:41:54 -0700

    fix : edit handle_register_sub func

--- registerpage.cpp
+++ registerpage.cpp
@@ -3,6 +3,7 @@
 #include "usermanager.h"
 #include "loginpage.h"
 #include <QMessageBox>
+#include <QRegularExpression>
 
 Dialogregisterpage::Dialogregisterpage(QWidget *parent)
     : QDialog(parent)
@@ -24,12 +25,35 @@ Dialogregisterpage::~Dialogregisterpage()
 }
 
 void Dialogregisterpage::handle_register_sub(){
+    //qDebug() << "Function called!";
+
     QString username = ui->lineEdit_username->text();
     QString password = ui->lineEdit_password->text();
     QString email = ui->lineEdit_email->text();
+    QString confirmPassword = ui->lineEdit_confirmPassword->text();
+
+    //qDebug() << "Password:" << password;
+    //qDebug() << "Confirm Password:" << confirmPassword;
+    //qDebug() << "Are they equal?" << (password == confirmPassword);
 
     if (username.isEmpty() || password.isEmpty()) {
         QMessageBox::warning(this, "Error", "Username and password cannot be empty!");
+        return;
+    }
+
+    if (password.length() < 8) {
+        QMessageBox::warning(this, "Error", "Password must be at least 8 characters!");
+        return;
+    }
+
+    if (password != confirmPassword) {
+        QMessageBox::warning(this, "Error", "Confirm password doesn't match!");
+        return;
+    }
+
+    QRegularExpression emailRegex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+    if (!emailRegex.match(email).hasMatch()) {
+        QMessageBox::warning(this, "Error", "Incorrect email format!");
         return;
     }
 
@@ -49,4 +73,6 @@ void Dialogregisterpage::handle_register_sub(){
     else {
         QMessageBox::warning(this, "Error", "Username already exists! Click Login to sign in.");
     }
+
+
 }

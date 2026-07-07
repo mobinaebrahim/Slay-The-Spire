b6d216b4fab1fed882c4c512dcf19d2b3a719b3e
dinabagherzadeh264-db <dinabagherzadeh264@gmail.com>
2026-07-07 22:10:31 +0330

    fix: upgrade funcs added & applyEffects funcs fixed and uncommented in Power Cards classes

--- PowerCard.cpp
+++ PowerCard.cpp
@@ -4,87 +4,70 @@
 PowerCard::PowerCard(CardType type, string name, string description, int cost)
 	: Card(type, name, description, cost) {}
 
-void PowerCard::applyEffect(class Character* caster, class Character* target) {
-    if (caster) {
-        //caster->decreaseEnergy(energyCost); 
-        //cout << caster->getName() << " spent " << energyCost << " energy to play " << name << ".\n";
-    }
-}
+void PowerCard::applyEffect(Character* caster, Character* target, BattleManager* bm) {}
 
 //_____________________________________InflameCard_____________________________________
 InflameCard::InflameCard(): PowerCard(CardType::Power, "Inflame",
     "Gain 2 Strength", 1) {}
 
-void InflameCard::applyEffect(class Character* caster, class Character* target) {
-    PowerCard::applyEffect(caster, target);
-    if (caster) {
-        //cout << "Inflame card played!\n";
-        //caster->applyStatus("Strength", 2); 
-        //cout << " -> You gained 2 strength\n";
-    }
+void InflameCard::upgrade() { Card::upgrade(); }
+
+void InflameCard::applyEffect(Character* caster, Character* target, BattleManager* bm) {
+    if (caster) 
+        caster->applyStatus(new StrengthEffect(isUpgraded ? 3 : 2));
 }
 
 //___________________________________MetallicizeCard___________________________________
 MetallicizeCard::MetallicizeCard() : PowerCard(CardType::Power, "Metallicize",
     "At the end of your turn get 3 block", 1) {}
 
-void MetallicizeCard::applyEffect(class Character* caster, class Character* target) {
-    PowerCard::applyEffect(caster, target);
-    if (caster) {
-        //cout << "Metallicize card played!\n";
-        // caster->applyStatus("Metallicize", 3);
-        cout << " -> Metallicize buff applied! (you will gain 3 block at end of your turn)\n";
-    }
+void MetallicizeCard::upgrade() { Card::upgrade(); }
+
+void MetallicizeCard::applyEffect(Character* caster, Character* target, BattleManager* bm) {
+    if (caster) 
+        caster->applyStatus(new MetallicizeEffect(isUpgraded ? 4 : 3));
 }
 
 //____________________________________DemonFormCard____________________________________
 DemonFormCard::DemonFormCard() : PowerCard(CardType::Power, "DemonForm",
     "At the start of your turn gain 3 Strength", 3) {}
 
-void DemonFormCard::applyEffect(class Character* caster, class Character* target) {
-    PowerCard::applyEffect(caster, target);
-    if (caster) {
-        //cout << "Demon Form card played!\n";
-        // caster->TurnStartEffect("DemonForm");
-        cout << " -> Demon Form applied! (You will gain 3 Strength at the start of each turn)\n";
-    }
+void DemonFormCard::upgrade() { Card::upgrade(); }
+
+void DemonFormCard::applyEffect(Character* caster, Character* target, BattleManager* bm) {
+    if (caster) 
+        caster->applyStatus(new DemonFormEffect(isUpgraded ? 3 : 2));
 }
 
 //____________________________________BrutalityCard____________________________________
 BrutalityCard::BrutalityCard() : PowerCard(CardType::Power, "Brutality",
     "At the start of your turn lose 1 HP and draw 1 card", 0) {}
 
-void BrutalityCard::applyEffect(class Character* caster, class Character* target) {
-    PowerCard::applyEffect(caster, target);
-    if (caster) {
-        //cout << "Brutality card played!\n";
-        // caster->TurnStartEffect("Brutality");
-        cout << " -> Brutality applied! (You will lose 1 HP and draw 1 card at the start of each turn)\n";
-    }
+void BrutalityCard::upgrade() { Card::upgrade(); }
+
+void BrutalityCard::applyEffect(Character* caster, Character* target, BattleManager* bm) {
+    if (caster) 
+        caster->TurnStartEffect("Brutality");   
 }
 
 //____________________________________FeelNoPainCard___________________________________
 FeelNoPainCard::FeelNoPainCard():PowerCard(CardType::Power, "FeelNoPain",
     "Every time a card is Exhausted, gain 3 block", 1) {}
 
-void FeelNoPainCard::applyEffect(class Character* caster, class Character* target) {
-    PowerCard::applyEffect(caster, target);
-    if (caster) {
-        //cout << "Feel No Pain card played!\n";
-        // caster->add_block_when_exhausted();
-        cout << " -> Feel No Pain applied! (You will gain 3 block every time a card is Exhausted)\n";
-    }
+void FeelNoPainCard::upgrade() { Card::upgrade(); }
+
+void FeelNoPainCard::applyEffect(Character* caster, Character* target, BattleManager* bm) {
+    //if(caster)
+        //caster->applyStatus(new FeelNoPainEffect(isUpgraded ? 4 : 3));
 }
 
 //____________________________________BarricadeCard____________________________________
 BarricadeCard::BarricadeCard():PowerCard(CardType::Power, "Barricade",
     "Block is not removed at the start of your turn", 3) {}
 
-void BarricadeCard::applyEffect(class Character* caster, class Character* target) {
-    PowerCard::applyEffect(caster, target);
-    if (caster) {
-        //cout << "Barricade card played!\n";
-        // caster->keep_block_until_next_turn();
-        cout << " -> Barricade applied! (Your block will not be removed at the start of your turn)\n";
-    }
+void BarricadeCard::upgrade() { Card::upgrade(); }
+
+void BarricadeCard::applyEffect(Character* caster, Character* target, BattleManager* bm) {
+    //if (caster) 
+        //caster->applyStatus(new BarricadeEffect());
 }
--- PowerCard.h
+++ PowerCard.h
@@ -2,6 +2,7 @@
 #define POWERCARD_H
 
 #include "Card.h"
+#include "BattleManager.h"
 #include <string>
 #include <iostream>
 
@@ -10,43 +11,50 @@ using namespace std;
 class PowerCard : public Card {
 public:
     PowerCard(CardType type, string name, string desc, int cost);
-    virtual void applyEffect(class Character* caster, class Character* target) override;
+    void upgrade() override;
+    virtual void applyEffect(class Character* caster, class Character* target, BattleManager* bm) override;
 };
 
 class InflameCard : public PowerCard {
 public:
     InflameCard();
-    void applyEffect(class Character* caster, class Character* target) override;
+    void upgrade() override;
+    void applyEffect(class Character* caster, class Character* target, BattleManager* bm) override;
 };
 
 class MetallicizeCard : public PowerCard {
 public:
     MetallicizeCard();
-    void applyEffect(class Character* caster, class Character* target) override;
+    void upgrade() override;
+    void applyEffect(class Character* caster, class Character* target, BattleManager* bm) override;
 };
 
 class DemonFormCard : public PowerCard {
 public:
     DemonFormCard();
-    void applyEffect(class Character* caster, class Character* target) override;
+    void upgrade() override;
+    void applyEffect(class Character* caster, class Character* target, BattleManager* bm) override;
 };
 
 class BrutalityCard : public PowerCard {
 public:
     BrutalityCard();
-    void applyEffect(class Character* caster, class Character* target) override;
+    void upgrade() override;
+    void applyEffect(class Character* caster, class Character* target, BattleManager* bm) override;
 };
 
 class FeelNoPainCard : public PowerCard {
 public:
     FeelNoPainCard();
-    void applyEffect(class Character* caster, class Character* target) override;
+    void upgrade() override;
+    void applyEffect(class Character* caster, class Character* target, BattleManager* bm) override;
 };
 
 class BarricadeCard : public PowerCard {
 public:
     BarricadeCard();
-    void applyEffect(class Character* caster, class Character* target) override;
+    void upgrade() override;
+    void applyEffect(class Character* caster, class Character* target, BattleManager* bm) override;
 };
 
 #endif

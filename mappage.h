91692b28e00b108ee4c26743224c8dce22678f1c
mobinaebrahim <mobinaebrahim1385@gmail.com>
2026-07-13 02:21:03 -0700

    feat : Display room names on map

--- mappage.h
+++ mappage.h
@@ -3,6 +3,7 @@
 
 #include <QDialog>
 #include <QTimer>
+#include <QMessageBox>
 #include "mapview.h"
 #include "gamemap.h"
 
@@ -16,6 +17,17 @@ public:
 private:
     MapView *m_mapView;
     GameMap *m_gameMap;
+
+    void handleRoomEntered(MapNode *node);
+
+
+    void openCombat(MapNode *node);
+    void openElite(MapNode *node);
+    void openEvent(MapNode *node);
+    void openShop(MapNode *node);
+    void openCampfire(MapNode *node);
+    void openTreasure(MapNode *node);
+    void openBossFight(MapNode *node);
 };
 
 #endif // MAPPAGE_H

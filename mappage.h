03bc549967db6f0537046a001c33a9fbaa50e371
mobinaebrahim <mobinaebrahim1385@gmail.com>
2026-07-13 00:29:09 -0700

    Create map page

---
+++ mappage.h
@@ -0,0 +1,20 @@
+#ifndef MAPPAGE_H
+#define MAPPAGE_H
+
+#include <QDialog>
+#include "mapview.h"
+#include "gamemap.h"
+
+class MapPage : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit MapPage(QWidget *parent = nullptr);
+    ~MapPage();
+
+private:
+    MapView *m_mapView;
+    GameMap *m_gameMap;
+};
+
+#endif // MAPPAGE_H

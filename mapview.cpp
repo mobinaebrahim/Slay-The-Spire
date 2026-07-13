235b3ebeb67d3a73b754b88d62b23f279ee0aa15
mobinaebrahim <mobinaebrahim1385@gmail.com>
2026-07-13 08:22:09 -0700

    feat : Set photo in map

--- mapview.cpp
+++ mapview.cpp
@@ -45,7 +45,7 @@ void MapView::buildScene(GameMap *map)
         }
     }
 
-    // Step 3: now draw the circles (on top of the lines)
+    /*// Step 3: now draw the circles (on top of the lines)
     for (int f = 0; f < map->floorCount(); ++f) {
         for (int i = 0; i < map->roomCountAt(f); ++i) {
             MapNode *node = map->nodeAt(f, i);
@@ -67,6 +67,48 @@ void MapView::buildScene(GameMap *map)
             item->setBrush(brush);
 
             connect(item, &MapNodeItem::clicked, this, &MapView::roomClicked);
+
+            m_scene->addItem(item);
+        }
+    }*/
+
+    // Step 3: draw the circles on top of the lines, colored by state, and clickable
+    for (int f = 0; f < map->floorCount(); ++f) {
+        for (int i = 0; i < map->roomCountAt(f); ++i) {
+            MapNode *node = map->nodeAt(f, i);
+            QPointF pos = m_positions[node];
+
+            QBrush brush;
+            if (node->available()) {
+                brush = QBrush(Qt::yellow);
+            } else if (node->visited()) {
+                brush = QBrush(Qt::darkGray);
+            } else {
+                brush = QBrush(Qt::lightGray);
+            }
+
+            MapNodeItem *item = new MapNodeItem(node,
+                                                pos.x() - nodeRadius, pos.y() - nodeRadius,
+                                                nodeRadius * 2, nodeRadius * 2);
+            item->setPen(QPen(Qt::black));
+            item->setBrush(brush);
+
+            connect(item, &MapNodeItem::clicked, this, &MapView::roomClicked);
+
+            // Add the room-type icon on top of the circle
+            QString iconPath = iconPathForRoomType(node->roomType());
+            QPixmap pix(iconPath);
+            if (!pix.isNull()) {
+                qreal iconSize = nodeRadius * 1.4; // slightly smaller than the circle
+
+                QGraphicsPixmapItem *iconItem = new QGraphicsPixmapItem(item); // child of the circle
+                QPixmap scaled = pix.scaled(iconSize, iconSize, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+                iconItem->setPixmap(scaled);
+
+                QRectF ellipseRect = item->rect();
+                iconItem->setPos(ellipseRect.center().x() - scaled.width() / 2.0,
+                                 ellipseRect.center().y() - scaled.height() / 2.0);
+            }
 
             m_scene->addItem(item);
         }

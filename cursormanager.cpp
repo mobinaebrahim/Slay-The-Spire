5af8630df66357b806a17cf1cbf018919be3c301
mobinaebrahim <mobinaebrahim1385@gmail.com>
2026-07-19 02:03:48 -0700

    feat : Implement pathforindex Func

--- cursormanager.cpp
+++ cursormanager.cpp
@@ -1,3 +1,18 @@
 #include "cursormanager.h"
+#include <QApplication>
+#include <QCursor>
+#include <QPixmap>
+#include <QSettings>
 
-CursorManager::CursorManager() {}
+QString CursorManager::pathForIndex(int index)
+{
+    switch (index) {
+    case 0: return ":/assets/mainmenu/cursor1.png";
+    case 1: return ":/assets/mainmenu/cursor2.png";
+    case 2: return ":/assets/mainmenu/cursor3.png";
+    default: return ":/assets/mainmenu/cursor1.png";
+    }
+}
+
+
+#endif

2b2351cd8a2798b0281aa9f6c7120f3341ae9669
mobinaebrahim <mobinaebrahim1385@gmail.com>
2026-07-20 03:21:57 -0700

    just try and cry :)

--- combatpage.cpp
+++ combatpage.cpp
@@ -5,43 +5,6 @@
 #include "PowerCard.h"
 #include "StatusCard.h"
 
-Card* createCardByName(const std::string& name) {
-    if (name == "Bash")            return new BashCard();
-    if (name == "Blood for Blood") return new Blood_for_BloodCard();
-    if (name == "Clash")           return new ClashCard();
-    if (name == "Feed")            return new FeedCard();
-    if (name == "Immolate")        return new ImmolateCard();
-    if (name == "PerfectedStrike") return new PerfectedStrike();
-    if (name == "Reaper")          return new ReaperCard();
-    if (name == "Strike")          return new AttackCard(CardType::Attack, "Strike", "Deal 6 damage", 1, 6, 9);
-    if (name == "Bludgeon")        return new AttackCard(CardType::Attack, "Bludgeon", "Deal 32 damage", 3, 32, 42);
-    if (name == "TwinStrike")      return new TwinStrikeCard();
-    if (name == "Whirlwind")       return new WhirlwindCard();
-    if (name == "Barricade")       return new BarricadeCard();
-    if (name == "Bloodletting")    return new BloodlettingCard();
-    if (name == "Brutality")       return new BrutalityCard();
-    if (name == "Burn")            return new BurnCard();
-    if (name == "CurseOfBell")     return new CurseOfBellCard();
-    if (name == "DualWield")       return new DualWieldCard();
-    if (name == "Daze")            return new DazeCard();
-    if (name == "Defend")          return new DefendCard();
-    if (name == "DemonForm")       return new DemonFormCard();
-    if (name == "Disarm")          return new DisarmCard();
-    if (name == "Entrench")        return new EntrenchCard();
-    if (name == "Exhume")          return new ExhumeCard();
-    if (name == "FeelNoPain")      return new FeelNoPainCard();
-    if (name == "Impervious")      return new ImperviousCard();
-    if (name == "Inflame")         return new InflameCard();
-    if (name == "JAX")             return new JAXCard();
-    if (name == "LimitBreak")      return new LimitBreakCard();
-    if (name == "Metallicize")     return new MetallicizeCard();
-    if (name == "Offering")        return new OfferingCard();
-    if (name == "Regret")          return new RegretCard();
-    if (name == "ShrugItOff")      return new ShrugItOffCard();
-    if (name == "Slime")           return new SlimeCard();
-    if (name == "Wound")           return new WoundCard();
-    return nullptr;
-}
 
 CombatPage::CombatPage(QWidget *parent, bool isLeader)
     : QWidget(parent)
@@ -158,26 +121,6 @@ void CombatPage::updateMyHandUI()
     }
 }
 
-void CombatPage::updateMyHandUI()
-{
-    QLayout *layout = m_myCardsContainer->layout();
-    QLayoutItem *child;
-    while ((child = layout->takeAt(0)) != nullptr) {
-        if (child->widget()) {
-            child->widget()->hide();
-            delete child->widget();
-        }
-        delete child;
-    }
-
-    const std::vector<Card*>& hand = m_localPlayer->getHand();
-    for (Card *card : hand) {
-        if (!card) continue;
-        QPushButton *cardBtn = new QPushButton(QString::fromStdString(card->getName()));
-        cardBtn->setFixedSize(100, 140);
-        layout->addWidget(cardBtn);
-    }
-}
 
 void CombatPage::handleNetworkMessage(const QJsonObject &obj)
 {
